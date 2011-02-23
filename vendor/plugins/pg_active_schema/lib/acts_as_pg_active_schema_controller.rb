0851e4639febd13f9b704e3613d1372034f11eb9
Mark Lorenz <markjlorenz@dapplebeforedawn.com>
2011-02-22 22:50:02 -0500

    v0.2

--- vendor/plugins/pg_active_schema/lib/acts_as_pg_active_schema_controller.rb
+++ vendor/plugins/pg_active_schema/lib/acts_as_pg_active_schema_controller.rb
@@ -1,3 +1,6 @@
+require 'active_record/base'
+require 'pg_active_schema'
+
 module ActsAsPgActiveSchemaController
   def self.included(base)
     base.extend(ClassMethods)
@@ -8,11 +11,11 @@ module ActsAsPgActiveSchemaController
       raise ArgumentError, "Hash expected, got #{options.class.name}" if not options.is_a?(Hash) and not options.empty?
       send :include, InstanceMethods
 
-#      rescue_from PgActiveSchema::NoSchema do |exception|
-#        respond_to do |format|
-#          format.html { redirect_to :controller=>:pg_active_schema_errors, :action=>:no_schema, :tenant=>current_tenant }
-#        end
-#      end
+      rescue_from PgActiveSchema::NoSchema do |exception|
+        respond_to do |format|
+          format.html { redirect_to :controller=>:pg_active_schema_errors, :action=>:no_schema, :tenant=>current_tenant }
+        end
+      end
     end
   end
 
